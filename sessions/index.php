f7ee632c0ef3d04edc3292151721019e545bfb97
Alex Hebing <a.hebing@prodemos.nl>
2023-08-27 16:25:01 +0200

    Update sessions and remove tracks

--- sessions/index.php
+++ sessions/index.php
@@ -1,58 +1,72 @@
 <?php
-	error_reporting(E_ALL);
+
+error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+$sessions = array();
+$mp3dir = "./mp3s";
 
-	include('edit/databaseHelper.php');
-	$db = new dataBaseContext();
-    $sessions = $db->listSessions();
+foreach (new DirectoryIterator($mp3dir) as $file) {
+  if ($file->isFile()) {
+    $title = basename($file);
+    $path = $mp3dir . '/' . $title;
+
+    $file = array(
+      "title" => basename($file, ".mp3"),
+      "path" => $path
+    );
+
+    array_push($sessions, $file);
+  }
+}
+
+    //include('master.php');
 ?>
 
 <html>
 <head>
 <head id="head">
-  <script src="http://apparatusrex.nl/scripts/jquery-3.1.1.js"></script>
+  <script src="scripts/jquery-3.1.1.js"></script>
   <title>Apparatus Rex</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="style.css">
-  <link rel="shortcut icon" href='http://apparatusrex.nl/img/round.ico' />
+  <link rel="shortcut icon" href='img/round.ico' />
 </head>
 
 <body>
 
 <div id="menu">
-	<script>
-		$('#menu').load('../menu.html');
-	</script>
+    <script>
+        $('#menu').load('../menu.html');
+    </script>
 </div>
 
 <div class="content">
   <img id="banner" src="../img/banner.png"/>
 
-	<table id="myTable">
+    <table id="myTable">
         <thead><tr>
-            <th>Sessions</th><th>Tempo</th><th>Boomfactor</th>
+            <th>Sessions</th><th>Play!</th>
         </tr></thead>
         <tbody data-bind="foreach: sessions">
           <tr>
               <td>
-              	<div class="tablecontent">              	
-	              	<img data-bind="attr: { 'src': plaatjePad }" width="120" heigth="120">
-	              	<div class="audioTitle">
-		              	<span data-bind="text: naam"></span>
-		              	<audio controls>
-        						  <source data-bind="attr: { 'src': audioPad }" type="audio/mpeg">
-        						  Your browser does not support the audio tag.
-        						</audio>
-        					</div>
-        				</div>
+                <p data-bind="text: title"></p>
               </td>
               <td>
-              	<span data-bind="text: tempo"></span>
+                  <div class="audioPlayer">
+                    <audio controls>
+                      <source data-bind="attr: { 'src': path }" type="audio/mpeg">
+                      Your browser does not support the audio tag.
+                    </audio>
+                  </div>
+              </td>
+              <!-- <td>
+                  <span data-bind="text: tempo"></span>
               </td>
               <td>
-              	<span data-bind="text: boomFactor"></span>
-              </td>
+                  <span data-bind="text: boomFactor"></span>
+              </td> -->
           </tr>    
         </tbody>
       </table>
@@ -79,9 +93,9 @@ ko.applyBindings(new SessionsViewModel());
 
 $(document).ready(function(){
     $('#myTable').DataTable({
-    	"paging": false,
-    	"searching": false,
-    	"info": false
+        "paging": false,
+        "searching": false,
+        "info": false
     });
 });
 
